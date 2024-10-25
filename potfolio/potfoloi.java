43d9e403b12052987d5406a3d5b21a36bde945c8
mathewebute <mathewebute001@gmail.com>
2024-10-25 19:19:53 +0100

    Add files via upload

---
+++ potfolio/potfoloi.java
@@ -0,0 +1,100 @@
+*
+{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+body
+{
+background-image: url("s.jpg") ;
+background-repeat: no-repeat;
+background-size: cover;
+}
+    .div {
+        background-image: url('po');
+        background-repeat: no-repeat;
+        background-size: cover;
+        height: 110vh;
+      }
+      
+
+
+
+.menu-bar{
+    background:;
+    text-align: right; /* Aligns text to the right edge */
+    padding-right: 70px; /* Removes padding from the right edge */
+}
+
+.menu-bar ul {
+    display: inline-flex;
+    list-style: none;
+    color: ff;
+}
+.menu-bar ul li{
+    width: 80px;
+    margin: 15px;
+    padding: 5px;
+}
+.menu-bar ul li a{
+    text-decoration: none;
+    color: fff;
+} 
+
+a {
+  color: white; /* Replace with your desired color */
+}
+.logo {
+    height: 140px;
+    width: 140px; /* logo width */
+    height: auto; /* maintain aspect ratio */
+    margin-right: 650px; /* spacing between logo and menu */
+}    
+
+  
+  nav ul li a{
+    color: #fff;
+    text-decoration: none;
+    font-size: 18px;
+    position: relative;
+
+  }
+  nav ul li a::after{
+    content: '';
+    width: 0%;
+    height: 3px;
+    background: #ff004f;
+    position: absolute;
+    left: 0;
+    bottom: -6px;
+    transition: 0.5s;
+
+  }
+  nav ul li a:hover:after{
+    width: 100%;
+
+  }
+ .header-text{
+    margin-top: 20%;
+    font-size: 30px;
+    align-items: center; /* vertical alignment: center */
+   
+
+   
+
+ }
+.header-text p{
+    font-size: 29px;
+    margin-top: 30px;
+    color: #fff;
+      
+}
+.header-text p span{
+color: #ff004f;
+
+}
+
+
+
+
+padding-left: 1px; /* adjust distance from left border */
